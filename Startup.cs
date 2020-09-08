bf5fda9721499397adc7d5674f8955e01ceef689
Mueni <4missmusau@gmail.com>
2020-09-08 12:56:36 +0300

    adding endpoints

--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,17 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MueniCrafts.Models;
 using MueniCrafts.Services;
 
 namespace MueniCrafts
@@ -52,6 +56,13 @@ namespace MueniCrafts
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapGet("products", (context) =>
+                {
+                    var products = app.ApplicationServices.GetService<JsonFileProductFile>().GetProducts();
+                    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
+
+                    return context.Response.WriteAsync(json);
+                });
             });
         }
     }
