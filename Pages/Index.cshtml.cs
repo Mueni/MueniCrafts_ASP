3b895bab752ed7414b3a1d220c3e9e4ff7a64fb1
Mueni <4missmusau@gmail.com>
2020-09-08 12:38:43 +0300

    Fron end changes

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-
+using MueniCrafts.Services;
+using MueniCrafts.Models;
 namespace MueniCrafts.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductFile ProductFile { get; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, 
+            JsonFileProductFile productFile)
         {
             _logger = logger;
+            ProductFile = productFile;
         }
 
         public void OnGet()
         {
-
+            Products = ProductFile.GetProducts();
         }
     }
 }
